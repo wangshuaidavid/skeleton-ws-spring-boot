24628a905fd4cb24126408474ee4cd2376f7969c
wangshuai <wangshuai@yeah.net>
2017-05-31 14:24:41 +0800

    fix

--- src/main/java/com/leanstacks/ws/SecurityConfiguration.java
+++ src/main/java/com/leanstacks/ws/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -73,6 +74,13 @@ public class SecurityConfiguration {
     public static class ApiWebSecurityConfigurerAdapter
             extends WebSecurityConfigurerAdapter {
 
+    	
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring()
+                .antMatchers("/h2/**");
+        }
+    	
         @Override
         protected void configure(final HttpSecurity http) throws Exception {
 
@@ -138,7 +146,8 @@ public class SecurityConfiguration {
     @Configuration
     public static class FormLoginWebSecurityConfigurerAdapter
             extends WebSecurityConfigurerAdapter {
-
+    	
+    	
         @Override
         protected void configure(final HttpSecurity http) throws Exception {
 
